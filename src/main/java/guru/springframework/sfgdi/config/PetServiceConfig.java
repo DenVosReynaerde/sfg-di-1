0a306d45cc6df0a236138a2c1a0955fc3f49827b
DenVosReynaerde <80472846+DenVosReynaerde@users.noreply.github.com>
2021-07-06 16:01:49 +0200

    further extention of Java configuration as example

---
+++ src/main/java/guru/springframework/sfgdi/config/PetServiceConfig.java
@@ -0,0 +1,29 @@
+package guru.springframework.sfgdi.config;
+
+import com.foxtrot.pets.CatPetService;
+import com.foxtrot.pets.PetService;
+import com.foxtrot.pets.PetServiceFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class PetServiceConfig {
+
+    @Bean
+    PetServiceFactory petServiceFactory() {
+        return new PetServiceFactory();
+    }
+
+    @Profile({"dog", "default"})
+    @Bean
+    PetService dogPetService(PetServiceFactory petServiceFactory) {
+        return petServiceFactory.getPetService("dog");
+    }
+
+    @Profile({"cat"})
+    @Bean
+    PetService catPetService(PetServiceFactory petServiceFactory) {
+        return petServiceFactory.getPetService("cat");
+    }
+}
